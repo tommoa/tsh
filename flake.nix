e0ac170c098baf3e6270952dd0d11dbc87e99b2e
Tom Almeida <tom@tommoa.me>
2026-01-14 01:18:59 +1100

    fix(nix): Ensure that nix build works correctly
    I have zig and zls installed by default in my nix systems, so I've been
    a bit lazy about keeping the nix build working and up-to-date. It's
    become a little stale, and some things weren't working (especially on
    Linux).
    
    One particular requirement is that of a target - otherwise we fail to
    compile on Linux. I've also renamed the devshell to `tsh`, so that `nix
    develop` gives nice output.

--- flake.nix
+++ flake.nix
@@ -8,18 +8,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, zig-overlay, zls, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      zig-overlay,
+      zls,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ zig-overlay.overlays.default ];
         };
+        zig = zig-overlay.packages.${system}."0.15.2";
+        target =
+          if pkgs.stdenv.isLinux then
+            "x86_64-linux-musl"
+          else if pkgs.stdenv.isDarwin then
+            "aarch64-macos"
+          else
+            "";
 
-        # Use master Zig since 0.15.2 is very recent
-        zig = pkgs.zigpkgs.master;
-
-      in {
+      in
+      {
         packages.default = pkgs.stdenvNoCC.mkDerivation {
           pname = "tsh";
           version = "0.0.0";
@@ -32,15 +46,18 @@
 
           buildPhase = ''
             mkdir -p .cache
-            zig build --cache-dir $(pwd)/.zig-cache --global-cache-dir $(pwd)/.cache -Doptimize=ReleaseSafe --prefix $out
+            zig build --cache-dir $(pwd)/.zig-cache --global-cache-dir $(pwd)/.cache -Doptimize=ReleaseSafe ${
+              if target != "" then "-Dtarget=${target}" else ""
+            } --prefix $out
           '';
         };
 
-        devShells.default = pkgs.mkShell {
+        devShells.tsh = pkgs.mkShell {
           buildInputs = [
             zig
             zls.packages.${system}.default
           ];
         };
-      });
+      }
+    );
 }
